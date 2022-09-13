dda45729b414fdc2db951ecd82b2b8859b3edb95
joselizagaravito <joselizagaravito@gmail.com>
2022-09-13 15:18:03 -0500

    avance de bootcoin

--- BootCoin/src/main/java/com/joseliza/bank/bootcoin/repository/EnchangeRateRepository.java
+++ BootCoin/src/main/java/com/joseliza/bank/bootcoin/repository/EnchangeRateRepository.java
@@ -1,5 +1,11 @@
 package com.joseliza.bank.bootcoin.repository;
 
-public interface EnchangeRateRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joseliza.bank.bootcoin.models.EnchangeRate;
+
+public interface EnchangeRateRepository extends JpaRepository<EnchangeRate, Long>{
+	public List<EnchangeRate> findByNumdocument(String numdocument);
 }
